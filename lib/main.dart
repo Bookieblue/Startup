9f8ac326dc7ea7d9ed406b06de3ba799e20d7c3d
Fasan_Oluwabukola <oluwabukolafasan@gmail.com>
2022-12-06 06:41:54 +0000

    sign up

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,154 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        // Application name
+        title: 'Flutter Hello World',
+        // Application theme data, you can set the colors for the application as
+        // you want
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // A widget which will be started on application startup
+        home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        body: Stack(children: [
+      Container(
+          child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            color: Colors.deepPurple,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            color: Colors.white,
+          )
+        ],
+      )),
+      Container(
+        padding: const EdgeInsets.fromLTRB(40, 80, 40, 50),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(6)),
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple.shade400, Colors.purple.shade200],
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              stops: [0.4, 0.7],
+              tileMode: TileMode.repeated,
+            ),
+          ),
+          child: Column(children: [
+            Icon(Icons.biotech_sharp, size: 90, color: Colors.white),
+            Text(
+              '\$09.99',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.none),
+            ),
+            Text(
+              'per month',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none),
+            ),
+            Divider(
+              indent: 20,
+              endIndent: 20,
+              thickness: 5,
+              color: Colors.pink[200],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'START UP',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.none),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Disk Space -200GB',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Bandwidth- 30GB',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Database -03',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Email Account -10',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '24/7 support - no',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'email support - yes',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none),
+            ),
+            SizedBox(height: 40),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.white,
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 25, color: Colors.deepPurple),
+              ),
+            ),
+          ]),
         ),
-      ),
-    );
+      )
+    ]));
   }
 }
